b6dd657658461e65acf714d3cc89dc8df71eeeab
Juan Carlos Munoz Rojas <juan.mrdev@gmail.com>
2022-11-23 22:47:41 -0500

    [UPDATE]

--- src/activity7/cal/Actividad7.java
+++ src/activity7/cal/Actividad7.java
@@ -1,5 +1,10 @@
 package activity7.cal;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Actividad7 {
@@ -25,8 +30,25 @@ public class Actividad7 {
                     new Calculator();
                 }
                 case 2 -> {
-                    System.out.println("Abrió reader file");
-                    new FileReader();
+                    try {
+                        Scanner scs = new Scanner(System.in);
+                        System.out.println("""
+                                Ingrese el nombre de un archivo .txt
+                                Puedes probar con el archivo llamado aguacate.
+                                """);
+                        String fileName = scs.nextLine();
+                        File myObj = new File("./src/activity7/cal/"+fileName+".txt");
+                        Scanner myReader = new Scanner(myObj);
+                        BufferedReader reader = new BufferedReader(new FileReader(myObj));
+                        while (myReader.hasNextLine()) {
+                            String data = myReader.nextLine();
+                            System.out.println(data);
+                        }
+                        myReader.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
                 }
             }
         } catch (Exception e){
--- src/activity7/cal/FileReader.java
+++
@@ -1,5 +0,0 @@
-package activity7.cal;
-
-public class FileReader {
-
-}
